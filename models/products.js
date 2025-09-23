cba3bccb9322c216b72bad4483b856364bcb8924
logrcia <lolagm04@gmail.com>
2025-09-23 15:36:17 -0300

    first commit

--- models/products.js
+++ models/products.js
@@ -2,7 +2,24 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: { type: String },
+  type: { type: String, required: true }, // ropa o musica
+
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: [String], required: true },
+  imageAlt: { type: [String] },
+
+  // solo para ropa
+  color: { type: [String] },
+  size: { type: [String] },
+  print: { type: [String] }, 
+  signed: { type: [Boolean], default: false }, 
+
+  // solo para música
+  cover: { type: [String] }, 
+
+  categories: [{ type: Schema.Types.ObjectId, ref:"Category"}]
 });
 
 export default mongoose.model("Product", productSchema, "Products");
--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,77 @@ const router = express.Router();
 import Product from "../models/products.js";
 
 const findAllProducts = async (req, res) => {
-
+    try {
+        const products = await Product.find().select("_id name categories description price image imageAlt color size print signed cover")
+        res.status(200).send({message: "Todos los productos", products: products})
+    } catch (error) {
+        return res.status(501).send({message: "Hubo error ", error})
+    }
 };
+
+const findOneProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        const product = await Product.findOne({_id: id}).select("id name categories description price image imageAlt color size print signed cover")
+        return res.status(200).send({message: "Producto encontrado ", product})
+    } catch (error) {
+        return res.status(501).send({message: "Hubo error ", error})
+    }
+}
+
+const addProduct = async (req, res) => {
+    const {type, name, categories, description, price, image, imageAlt, color, size, print, signed, cover} = req.body
+    try {
+        const product = new Product({type, name, categories, description, price, image, imageAlt, color, size, print, signed, cover})
+        await product.save()
+        return res.status(200).send({message: "Producto creado ", product})
+    } catch (error) {
+        return res.status(501).send({message: "Hubo error ", error})
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        const productToDelete = await Product.findOne({_id: id})
+
+        if(!productToDelete){
+            return res.status(404).send({message: "No existe el producto ", id: id})
+        }
+
+        await Product.deleteOne({_id: id})
+        return res.status(200).send({message: "Producto borrado", product: productToDelete})
+    } catch (error) {
+        return res.status(501).send({message: "Hubo error ", error})
+    }
+}
+
+const updateProduct = async (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    try {
+        const productToUpdate = await Product.findOne({_id: id})
+
+        if(!productToUpdate){
+            return res.status(404).send({message: "No existe el producto ", id: id})
+        }
+
+        //valores a actualizar
+        productToUpdate.name = name
+
+        await productToUpdate.save({_id: id})
+        return res.status(200).send({message: "Producto actualizado", product: productToUpdate})
+    } catch (error) {
+        return res.status(501).send({message: "Hubo error ", error})
+    }
+}
 
 
 //CRUD endpoints
 router.get("/", findAllProducts);
+router.get("/:id", findOneProduct);
+router.post("/", addProduct);
+router.delete("/:id", deleteProduct);
+router.put("/:id", updateProduct)
 
 export default router;
